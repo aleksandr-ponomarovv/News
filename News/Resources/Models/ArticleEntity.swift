7f0a4d114f66122df7732809c8a68812091dd51e
Oleksandr Ponomarov <45709081+aleksandr-ponomarovv@users.noreply.github.com>
2022-09-29 03:33:44 +0300

    changed model for realm

--- News/Resources/Models/ArticleEntity.swift
+++ News/Resources/Models/ArticleEntity.swift
@@ -5,7 +5,8 @@
 //  Created by Aleksandr on 26.09.2022.
 //
 
-import Foundation
+import RealmSwift
+import Realm
 
 // MARK: - ArticleEntity
 final class ArticleEntity: Decodable {
@@ -21,17 +22,20 @@ final class ArticleEntity: Decodable {
 }
 
 // MARK: - Article
-final class Article: ArticleTableViewCellModel, Decodable {
-    let source: Source
-    let author: String?
-    let title, articleDescription: String
-    let url: String
-    let urlToImage: String
-    let publishedAt: String
-    let content: String
+@objcMembers final class Article: Object, ArticleTableViewCellModel, Decodable {
+    dynamic var url: String = ""
     
-    var sourceName: String {
-        source.name
+    dynamic var source: Source? = .init()
+    dynamic var author: String? = ""
+    dynamic var title: String = ""
+    dynamic var articleDescription: String = ""
+    dynamic var urlToImage: String = ""
+    dynamic var publishedAt: String = ""
+    dynamic var content: String = ""
+    dynamic var isFavorite: Bool = false
+    
+    var sourceName: String? {
+        source?.name
     }
     
     var date: String? {
@@ -44,6 +48,10 @@ final class Article: ArticleTableViewCellModel, Decodable {
         case url, urlToImage, publishedAt, content
     }
 
+    override class func primaryKey() -> String? {
+        return "url"
+    }
+    
     init(source: Source, author: String?, title: String, articleDescription: String, url: String, urlToImage: String, publishedAt: String, content: String) {
         self.source = source
         self.author = author
@@ -54,20 +62,66 @@ final class Article: ArticleTableViewCellModel, Decodable {
         self.publishedAt = publishedAt
         self.content = content
     }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        url = try container.decode(String.self, forKey: .url)
+        source = try container.decode(Source.self, forKey: .source)
+        author = try? container.decode(String.self, forKey: .author)
+        title = try container.decode(String.self, forKey: .title)
+        articleDescription = try container.decode(String.self, forKey: .articleDescription)
+        urlToImage = try container.decode(String.self, forKey: .urlToImage)
+        publishedAt = try container.decode(String.self, forKey: .publishedAt)
+        content = try container.decode(String.self, forKey: .content)
+    }
+    
+    required override init() {
+        super.init()
+    }
+
+    required init(value: Any, schema: RLMSchema) {
+        super.init()
+    }
+
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init()
+    }
 }
 
 // MARK: - Source
-final class Source: Decodable {
-    let identifire: String?
-    let name: String
+@objcMembers final class Source: Object, Decodable {
+    dynamic var identifire: String? = ""
+    dynamic var name: String = ""
     
     enum CodingKeys: String, CodingKey {
         case identifire = "id"
         case name
     }
 
+    override class func primaryKey() -> String? {
+        return "name"
+    }
+    
     init(identifire: String?, name: String) {
         self.identifire = identifire
         self.name = name
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        identifire = try? container.decode(String.self, forKey: .identifire)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    required override init() {
+        super.init()
+    }
+
+    required init(value: Any, schema: RLMSchema) {
+        super.init()
+    }
+
+    required init(realm: RLMRealm, schema: RLMObjectSchema) {
+        super.init()
     }
 }
--- News/Resources/UI/ArticleCell/ArticleTableViewCell.swift
+++ News/Resources/UI/ArticleCell/ArticleTableViewCell.swift
@@ -10,9 +10,10 @@ import UIKit
 protocol ArticleTableViewCellModel {
     var author: String? { get }
     var title: String { get }
-    var sourceName: String { get }
+    var sourceName: String? { get }
     var date: String? { get }
     var urlToImage: String { get }
+    var isFavorite: Bool { get set }
 }
 
 final class ArticleTableViewCell: UITableViewCell {
@@ -24,12 +25,15 @@ final class ArticleTableViewCell: UITableViewCell {
     @IBOutlet private weak var iconImageView: UIImageView!
     @IBOutlet private weak var favoriteButton: UIButton!
     
+    var favoriteButtonCompletion: (() -> Void)?
+    
     var model: ArticleTableViewCellModel? {
         willSet(model) {
             authorLabel.text = model?.author
             titleLabel.text = model?.title
             sourceLabel.text = model?.sourceName
             dateLabel.text = model?.date
+            setupFavoriteButton(isFavorite: model?.isFavorite ?? false)
             if let urlString = model?.urlToImage, !urlString.isEmpty {
                 iconImageView.downloadImage(urlString: urlString)
             }
@@ -50,6 +54,18 @@ final class ArticleTableViewCell: UITableViewCell {
     }
     
     @IBAction private func didTapFavoriteButton(_ sender: UIButton) {
+        guard let isFavorite = model?.isFavorite else { return }
         
+        model?.isFavorite = !isFavorite
+        setupFavoriteButton(isFavorite: !isFavorite)
+        favoriteButtonCompletion?()
+    }
+}
+
+// MARK: - Private methods
+private extension ArticleTableViewCell {
+    func setupFavoriteButton(isFavorite: Bool) {
+        let image = isSelected ? UIImage(systemName: "bookmark.fill") : UIImage(systemName: "bookmark")
+        favoriteButton.setImage(image, for: .normal)
     }
 }
